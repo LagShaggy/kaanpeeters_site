50f290d7b993fb695425741c71e3168823c3e757
Filip Dombos <filip.dombos@gmail.com>
2023-08-13 15:04:18 +0200

    change naming of the components for better redablilty by removing ...Comp... from naming

---
+++ src/lib/components/UI/EventComp/EventContainer.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import EventItem from './EventItem.svelte';
+</script>
+
+<div class="flex flex-col overflow-auto gap-0.5">
+	{#each Array(2) as _, index (index)}
+		<EventItem />
+	{/each}
+	{#each Array(3) as _, index (index)}
+		<EventItem isPriority={false} />
+	{/each}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import BasePage from '$lib/components/UI/BasePage.svelte';
 	import GalleryContainer from '$lib/components/UI/Gallery/GalleryContainer.svelte';
-	import EventCompContainer from '$lib/components/UI/EventComp/EventCompContainer.svelte';
+	import EventContainer from '$lib/components/UI/EventComp/EventContainer.svelte';
 	import Biography from '$lib/components/UI/Biography.svelte';
-	
+
 	type BasePageProps = {
 		title: string;
 		subtitle: string;
@@ -17,14 +17,13 @@
 <BasePage {props}>
 	<div class="flex flex-col sm:flex-row flex-wrap">
 		<div class="w-full sm:w-1/2">
-			<Biography />		
+			<Biography />
 		</div>
 		<div class="w-full sm:w-1/2">
-			<EventCompContainer />
+			<EventContainer />
 		</div>
 		<div class="mt-5">
 			<GalleryContainer />
 		</div>
 	</div>
 </BasePage>
-
