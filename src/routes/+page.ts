5176de17f8ed566fdef324c70c2bd160138dee4d
Filip Dombos <filip.dombos@gmail.com>
2023-08-23 14:59:33 +0200

    maybe fix redirect

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,8 +1,6 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
-export const csr = true;
-
 export const load: PageLoad = () => {
 	throw redirect(301, '/de');
 };
