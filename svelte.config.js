e475a57f447b1913e5e0ebcdc3240006e51bda43
Filip Dombos <filip.dombos@gmail.com>
2023-08-23 14:51:16 +0200

    found the bug?

--- svelte.config.js
+++ svelte.config.js
@@ -5,22 +5,19 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess({
-		// default options are shown. On some platforms
-		// these options are set automatically — see below
-		pages: 'build',
-		assets: 'build',
-		fallback: '200.html',
-		precompress: false,
-		strict: true
-	}),
-
+	preprocess: vitePreprocess(),
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: '200.html'
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: '200.html',
+			precompress: false,
+			strict: true
 		}),
 		alias: {
 			img: 'src/lib/images/*',
