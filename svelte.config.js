fa725cb8ebacdbcbd0474d939d18b2688129c2cc
Filip Dombos <filip.dombos@gmail.com>
2023-08-23 13:40:03 +0200

    tinker with svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ const config = {
 		// these options are set automatically — see below
 		pages: 'build',
 		assets: 'build',
-		fallback: 'index.html',
+		fallback: '200.html',
 		precompress: false,
 		strict: true
 	}),
@@ -19,10 +19,9 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({ fallback: 'index.html' }),
+		adapter: adapter(),
 		alias: {
 			img: 'src/lib/images/*',
-
 			// an alias ending /* will only match
 			// the contents of a directory, not the directory itself
 			'my-directory/*': 'path/to/my-directory/*'
