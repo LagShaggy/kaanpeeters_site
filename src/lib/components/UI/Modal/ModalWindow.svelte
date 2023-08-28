3a0b711d4ef4cb8f9b3d0f86a1e7c270023b60bb
Dorian Janzetic <dorian.janzetic@gmail.com>
2023-08-28 19:53:31 +0200

    writings mobile view

--- src/lib/components/UI/Modal/ModalWindow.svelte
+++ src/lib/components/UI/Modal/ModalWindow.svelte
@@ -16,18 +16,17 @@
 {#if isModal}
 	<button
 		on:click={toggleModal}
-		class="fixed bg-opacity-25 backdrop-blur bg-gray-300 top-0 left-0 w-full h-full z-10"
+		class="bg-opacity-25 backdrop-blur bg-gray-300 w-full h-full fixed top-0 left-0 right-0 z-50 overflow-scroll"
 	>
 		<!-- svelte-ignore a11y-no-static-element-interactions -->
 		<!-- svelte-ignore a11y-click-events-have-key-events -->
-		<div
-			on:click|stopPropagation
-			class="cursor-default fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2"
-		>
-			<button class="absolute right-2 top-2" on:click={toggleModal}>
-				<img src={x_icon} alt="" />
-			</button>
-			<slot name="modal" />
+		<div on:click|stopPropagation class="flex justify-center items-center">
+			<div class="relative w-full sm:w-3/5">
+				<button class="absolute right-2 top-2 sm:top-12" on:click={toggleModal}>
+					<img src={x_icon} alt="" />
+				</button>
+				<slot name="modal" />
+			</div>
 		</div>
 	</button>
 {/if}
--- src/lib/components/UI/Writings/PoemView.svelte
+++ src/lib/components/UI/Writings/PoemView.svelte
@@ -1,21 +1,22 @@
 <script lang="ts">
-	import scrollBackgroud from '$assets/scroll_bg.jpg';
+	import scrollBackground from '$assets/scroll_bg.jpg';
 	import type { Poem } from '$lib/types';
 	import PoemFont from '../Elements/PoemFont.svelte';
 	export let poem: Poem;
 	export let downloadPdf: () => {};
 </script>
 
-<div class="overflow-y-visible">
-	<img src={scrollBackgroud} alt="Pergament Poem Background" />
-	<span class="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 text-black">
-		<PoemFont className="text-xl">
+<div class="bg-cover text-black" style="background-image: url({scrollBackground});">
+	<div class="mx-2 sm:m-10">
+		<br /> <br />
+		<PoemFont className="text-xl font-semibold">
 			{@html poem.name}
 		</PoemFont>
 		<br /> <br />
 		<PoemFont className="text-lg">
 			{@html poem.text}
 		</PoemFont>
-		<!-- <button on:click={downloadPdf}> CLICK ME </button> -->
-	</span>
+		<br /> <br />
+	</div>
+	<!-- <button on:click={downloadPdf}> CLICK ME </button> -->
 </div>
