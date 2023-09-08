67818779d8ea5872a2413aeb1a769fe1ac6de725
Dorian Janzetic <dorian.janzetic@gmail.com>
2023-09-08 09:58:46 +0200

    progress on fixing link navigation

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,5 +2,6 @@ import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async () => {
+	console.log('throw redirect to home');
 	throw redirect(301, '/de/home');
 };
--- src/routes/en/+page.ts
+++ src/routes/en/+page.ts
@@ -2,5 +2,6 @@ import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async () => {
+	console.log('throw redirect to en home');
 	throw redirect(301, '/en/home');
 };
