6dedfa7cc285fe64bc6b8053a0d593abf4d4e0e9
Filip Dombos <filip.dombos@gmail.com>
2023-09-17 15:44:48 +0200

    remove comment in footer

--- src/lib/components/UI/BasePage/PageShell.svelte
+++ src/lib/components/UI/BasePage/PageShell.svelte
@@ -8,11 +8,7 @@
 	<div class="lg:px-52">
 		<slot />
 	</div>
-	<!--
-	<div class=" flex justify-center">
-		<img src={notes} alt="musical notes" class="h-32" />
-	</div>
-	-->
+
 	<div class="mt-auto pt-5 bg-gray-900">
 		<slot name="footer" />
 	</div>
